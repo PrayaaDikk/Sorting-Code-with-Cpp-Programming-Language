7469dbcdf54e41e1c85c4ba43330637e90d6e46a
Fadil Prayadika <151795256+PrayaaDikk@users.noreply.github.com>
2024-01-11 11:37:51 +0800

    Add files via upload

---
+++ Sorting Code.cpp
@@ -0,0 +1,39 @@
+
+//This source code is for sorting numbers smallest to largest
+
+#include <iostream>
+using namespace std;
+int main(){
+    int amountOfData, temp;
+    cout << "Enter the amount of data : "; cin >> amountOfData;
+
+    int x[amountOfData];
+    for(int i = 0; i < amountOfData; i++){
+        cout << "Input the data --> "; cin >> x[i];
+    }
+
+    //before sorting process
+    cout << "\n\nBefore sorting\n";
+    for(int i = 0; i < amountOfData; i++){
+        cout << x[i] << " ";
+    }
+
+    //sorting formula
+    for(int i = 0; i < amountOfData; i++){
+        for(int j = 0; j < amountOfData; j++){
+            if(x[j] > x[j + 1]){
+                temp = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp;
+            }
+        }
+    }
+
+        //after sorting formula
+        cout << "\nAfter sorting\n";
+        for(int i = 0; i < amountOfData; i++){
+            cout << x[i] << " ";
+        }
+
+    return 0;
+}
